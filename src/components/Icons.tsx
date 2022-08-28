a407b0969ab5c44a9c9285d3b81b125ca2f21452
Duy Tran <duyvutran2303@gmail.com>
2022-08-27 23:21:39 -0400

    feat: add mobile menu

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -35,3 +35,43 @@ export function SunIcon(props: IconProps) {
     </svg>
   );
 }
+
+export function Bar3Icon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
+
+export function XMarkIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import cn from "classnames";
 import { useRouter } from "next/router";
 import { MoonIcon, SunIcon } from "./Icons";
 import { useTheme } from "next-themes";
+import { Bar3Icon, XMarkIcon } from "./Icons";
 
 type NavItemProps = {
   href: string;
@@ -33,6 +34,7 @@ const NavItem: React.FC<NavItemProps> = ({ href, text }) => {
 const Navbar = () => {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
+  const [isOpened, setOpen] = useState<boolean>(false);
 
   // After mounting, we have access to the theme
   useEffect(() => setMounted(true), []);
@@ -41,27 +43,83 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="sticky top-0 flex items-center justify-center w-full py-4 bg-slate-100 bg-opacity-40 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-40 dark:backdrop-blur-sm">
-      <div className="flex justify-between items-center max-w-2xl w-full px-4">
-        <div className="flex items-center space-x-2">
-          <Link href="/">
-            <a className="font-semibold text-xl mr-4 ">Duy Tran</a>
-          </Link>
-          <NavItem href="/courses" text="Courses" />
-          <NavItem href="/projects" text="Projects" />
-          <NavItem href="/blog" text="Blog" />
-          <NavItem href="/goodies" text="Goodies" />
-        </div>
+    <>
+      <nav className="sticky top-0 flex items-center justify-center w-full py-2 bg-slate-100 bg-opacity-40 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-40 dark:backdrop-blur-sm z-10">
+        <div className="flex justify-between items-center max-w-2xl w-full px-4">
+          <div className="flex items-center space-x-2">
+            <Link href="/">
+              <a className="font-semibold text-xl mr-4 ">Duy Tran</a>
+            </Link>
+            <NavItem href="/courses" text="Courses" />
+            <NavItem href="/projects" text="Projects" />
+            <NavItem href="/blog" text="Blog" />
+            <NavItem href="/goodies" text="Goodies" />
+          </div>
+          <div className="flex space-x-4">
+            <button
+              type="button"
+              className="w-9 h-9 flex items-center justify-center rounded-md bg-slate-300 hover:ring-2 ring-gray-400 dark:bg-slate-700 dark:ring-slate-500"
+              onClick={() =>
+                setTheme(resolvedTheme === "dark" ? "light" : "dark")
+              }
+            >
+              {resolvedTheme === "dark" ? (
+                <SunIcon fill="#FFFFFF" />
+              ) : (
+                <MoonIcon />
+              )}
+            </button>
 
-        <button
-          type="button"
-          className="w-9 h-9 flex items-center justify-center rounded-lg bg-slate-300 hover:ring-2 ring-gray-400 dark:bg-slate-700 dark:ring-slate-500"
-          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-        >
-          {resolvedTheme === "dark" ? <SunIcon fill="#FFFFFF" /> : <MoonIcon />}
-        </button>
+            <button
+              onClick={() => setOpen(!isOpened)}
+              className="w-9 h-9 flex items-center justify-center rounded-md border visible md:hidden border-slate-400 dark:border-slate-300"
+            >
+              {isOpened ? <XMarkIcon /> : <Bar3Icon />}
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div
+        className={cn(
+          isOpened ? "translate-x-0 " : "translate-x-full",
+          "fixed h-screen w-screen z-10 left-0 visible md:hidden flex flex-col bg-slate-100 bg-opacity-40 backdrop-blur-sm dark:bg-slate-800 dark:bg-opacity-40 dark:backdrop-blur-sm ease-in-out duration-300",
+          "py-4 px-8 divide-y divide-slate-400 dark:divide-slate-600"
+        )}
+      >
+        <Link href="/courses" passHref>
+          <a
+            className="font-semibold text-lg py-4"
+            onClick={() => setOpen(false)}
+          >
+            Courses
+          </a>
+        </Link>
+        <Link href="/projects" passHref>
+          <a
+            className="font-semibold text-lg py-4"
+            onClick={() => setOpen(false)}
+          >
+            Projects
+          </a>
+        </Link>
+        <Link href="/blog" passHref>
+          <a
+            className="font-semibold text-lg py-4"
+            onClick={() => setOpen(false)}
+          >
+            Blog
+          </a>
+        </Link>
+        <Link href="/goodies" passHref>
+          <a
+            className="font-semibold text-lg py-4"
+            onClick={() => setOpen(false)}
+          >
+            Goodies
+          </a>
+        </Link>
       </div>
-    </nav>
+    </>
   );
 };
 
